6f599016c7118ddf068afdeb8843a245f19241e5
Theos Mariani <theos.mariani@gmail.com>
2019-12-05 19:28:16 +0100

    init service profil and matching

---
+++ matching/src/main/java/com/n2i/polytech/argent/matching/MatchingApplication.java
@@ -0,0 +1,13 @@
+package com.n2i.polytech.argent.matching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchingApplication.class, args);
+    }
+
+}
